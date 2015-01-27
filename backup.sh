7fd250398b32aa083c0e1d5ea9978320e172cb35
aitmlouk <aitmlouk@aitmlouk>
2015-01-27 13:37:38 +0000

    this scripts is about: starting server automatically, backup all databases automatically clean old databases automatically

---
+++ backup.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+### BEGIN INIT INFO
+# Provides : Ait-Mlouk Addi (aitmlouk@gmail.com)     
+# site web : http://aitmlouk.esy.es/
+# blog     : http://aitmlouk-addi.blogspot.com/
+# linkedin : http://ma.linkedin.com/pub/addi-ait-mlouk/56/850/32a
+# viadeo   : http://ma.viadeo.com/en/profile/addi.mlk
+# Google+  : https://plus.google.com/+AitMloukAddi
+# Gmail    : aitmlouk@gmail.com
+### END INIT INFO
+
+#beginning of script
+#select all databases
+#archive all databases by this name (date_hour)
+#
+ DBNAME=`psql -Uopenerp -tq -dtemplate1 -c "select datname from pg_database"`
+ hours=$(date +%H-%M)
+ for DB in $DBNAME
+ do
+ BACKUPFILE=/opt/backup/$DB\_$hours\_`/bin/date +%D |sed 's;/;-;g'`
+ if  [ "$DB" != "template0" ] && [ "$DB" != "template1" ]; then
+ /usr/bin/vacuumdb --analyze -Uopenerp $DB
+ /usr/bin/pg_dump -Uopenerp --column-inserts $DB | gzip -c > $BACKUPFILE.out.gz
+ fi
+ done
+
+#End of script
---
+++ clean.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+### BEGIN INIT INFO
+# Provides : Ait-Mlouk Addi (aitmlouk@gmail.com)     
+# site web : http://aitmlouk.esy.es/
+# blog     : http://aitmlouk-addi.blogspot.com/
+# linkedin : http://ma.linkedin.com/pub/addi-ait-mlouk/56/850/32a
+# viadeo   : http://ma.viadeo.com/en/profile/addi.mlk
+# Google+  : https://plus.google.com/+AitMloukAddi
+# Gmail    : aitmlouk@gmail.com
+### END INIT INFO
+
+#beginning of script
+#Decoration of the full path to the backup directory
+#find all the files which are modified more than 10 days back
+#find $BACKUP_DIR -type f -mtime +10 -exec rm -f {} \;
+
+BACKUP_DIR=/opt/backup
+find $BACKUP_DIR -type f -mtime +10 -exec rm -f {} \;
+
+#End script
